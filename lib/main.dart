e0d1e87b8b132bd0b7c9f2a757b6b48b6792f6b2
Ibraheemahmed5 <92911409+ibraheemahmed48@users.noreply.github.com>
2023-02-25 01:55:41 +0300

    Edit firebase News page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:notificationsystem_mobile/test2.dart';
 
-void main() {
+import 'View/home_page.dart';
+import 'help/Colors.dart';
+import 'test.dart';
+
+main() async {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colorsapp.mainColor, // navigation bar color
+    statusBarColor: Colorsapp.mainColor, // status bar color
+  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -10,37 +24,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+    return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+
+        home: HomePage()
     );
   }
 }
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-
