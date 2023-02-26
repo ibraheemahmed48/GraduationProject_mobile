b2763f20c3554441b26144a0496ed45ed010ebea
Ibraheemahmed5 <92911409+ibraheemahmed48@users.noreply.github.com>
2023-02-27 01:01:55 +0300

    #5Edit firebase News page

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+
         debugShowCheckedModeBanner: false,
 
         home: HomePage()
